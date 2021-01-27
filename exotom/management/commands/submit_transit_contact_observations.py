cb285839240deaf326a87797f9d81b25b316c332
Tilman Masur <t.masur@stud.uni-goettingen.de>
2021-01-27 14:35:09 +0100

    submission of transit contacts (ie ingress/egress) by itself

---
+++ exotom/management/commands/submit_transit_contact_observations.py
@@ -0,0 +1,107 @@
+import traceback
+
+from django.core.management.base import BaseCommand
+
+from exotom.settings import FACILITIES, SITES
+from exotom.ofi.iagtransit import IAGTransitForm, IAGTransitSingleContactForm
+from tom_iag.iag import IAGFacility, get_instruments
+from exotom.models import Target, Transit
+
+from exotom.transits import calculate_transits_during_next_n_days
+from exotom.exposure_calculator import calculate_exposure_time
+from astropy.time import Time
+import pytz
+
+
+class Command(BaseCommand):
+    help = "Submit all transits that can be observed in the following night to telescope scheduler."
+
+    def handle(self, *args, **options):
+        submit_all_transit_contacts()
+
+
+def submit_all_transit_contacts():
+    now = Time.now()
+
+    targets = Target.objects.all()
+
+    instruments = get_instruments()
+
+    for target in targets:
+
+        calculate_transits_during_next_n_days(target, n_days=1)
+
+        transits_for_target = Transit.objects.filter(
+            target=target, start__gt=now.datetime.astimezone(pytz.utc)
+        )
+
+        for site_name, site_info in SITES.items():
+            instrument_type = site_info["instrument"]
+            instrument_details = instruments[instrument_type]
+
+            for transit in transits_for_target:
+                submit_all_ingresses_egresses(
+                    instrument_details, instrument_type, site_name, transit
+                )
+
+
+def submit_all_ingresses_egresses(
+    instrument_details, instrument_type, site_name, transit
+):
+    if transit.ingress_observable_at_site(site=site_name):
+        try:
+            submit_transit_single_contact_to_instrument(
+                transit, instrument_type, instrument_details, contact="ingress"
+            )
+        except Exception as e:
+            print(f"Error when submitting transit observation to instrument")
+            print(traceback.format_exc())
+
+    if transit.egress_observable_at_site(site=site_name):
+        try:
+            submit_transit_single_contact_to_instrument(
+                transit, instrument_type, instrument_details, contact="egress"
+            )
+        except Exception as e:
+            print(f"Error when submitting transit observation to instrument")
+            print(traceback.format_exc())
+
+
+def submit_transit_single_contact_to_instrument(
+    transit: Transit, instrument_type: str, instrument_details: dict, contact: str
+):
+    print(
+        f"Submitting transit {contact.upper()} {transit} with transit id {transit.id} and target id {transit.target_id}"
+    )
+
+    observation_data = get_observation_data(
+        transit, instrument_type, instrument_details, contact
+    )
+    form = IAGTransitSingleContactForm(initial=observation_data, data=observation_data)
+    form.is_valid()
+    facility = IAGFacility()
+    facility.submit_observation(form.observation_payload())
+
+
+def get_observation_data(
+    transit: Transit, instrument_type: str, instrument_details: dict, contact: str
+) -> dict:
+    magnitude = transit.target.targetextra_set.get(key="Mag (TESS)").float_value
+    exposure_time = calculate_exposure_time(magnitude)
+
+    data = {
+        "facility": "IAGTransit",
+        "instrument_type": instrument_type,
+        "transit": transit.number,
+        "contact": contact,
+        "target_id": transit.target_id,
+        "ipp_value": get_ipp_value(transit),
+        "exposure_time": exposure_time,
+        "readout_mode": instrument_details["modes"]["readout"]["modes"][0]["code"],
+        "filter": instrument_details["optical_elements"]["filters"][0]["code"],
+    }
+    return data
+
+
+def get_ipp_value(transit: Transit) -> float:
+    return 1.05
--- exotom/ofi/iagtransit.py
+++ exotom/ofi/iagtransit.py
@@ -180,6 +180,179 @@ class IAGTransitForm(IAGImagingObservationForm):
         return payload
 
 
+class IAGTransitSingleContactForm(IAGImagingObservationForm):
+    contact = forms.CharField(max_length=20)
+
+    def __init__(self, *args, **kwargs):
+        IAGImagingObservationForm.__init__(self, *args, **kwargs)
+
+        # transit field
+        transits = []
+        for c in self.transit_contact_choices():
+            if c[0] == int(self.initial["transit"]):
+                transits.append(c)
+        self.fields["transit"] = forms.ChoiceField(choices=transits)
+
+    def layout(self):
+        return Div(
+            Div(
+                Div(
+                    "transit",
+                    css_class="col",
+                ),
+                css_class="row",
+            ),
+            Div(
+                Div(
+                    "instrument_type",
+                    css_class="col",
+                ),
+                css_class="row",
+            ),
+            Div(
+                Div("readout_mode", css_class="col"),
+                Div("filter", css_class="col"),
+                css_class="form-row",
+            ),
+            Div(
+                Div("exposure_time", css_class="col"),
+                Div("ipp_value", css_class="col"),
+                css_class="form-row",
+            ),
+        )
+
+    def transit_contact_choices(self):
+        now = Time.now().isot
+
+        contact_choices = []
+        contact_choices.extend(
+            [
+                (
+                    t.number,
+                    "#%d: %s Ingress" % (t.number, t.start.strftime("%Y/%m/%d %H:%M")),
+                )
+                for t in Transit.objects.filter(start__gt=now)
+                if t.ingress_visible(self.initial["facility"])
+            ]
+        )
+
+        contact_choices.extend(
+            [
+                (
+                    t.number,
+                    "#%d: %s Egress" % (t.number, t.start.strftime("%Y/%m/%d %H:%M")),
+                )
+                for t in Transit.objects.filter(start__gt=now)
+                if t.egress_visible(self.initial["facility"])
+            ]
+        )
+
+        contact_choices = sorted(contact_choices, key=lambda x: x[1])
+        return contact_choices
+
+    def clean_start(self):
+        start = self.cleaned_data["start"]
+        return parse(start).isoformat()
+
+    def clean_end(self):
+        end = self.cleaned_data["end"]
+        return parse(end).isoformat()
+
+    def _build_acquisition_config(self):
+        acquisition_config = {"mode": "ON"}
+
+        return acquisition_config
+
+    def _build_guiding_config(self):
+        guiding_config = {"mode": "ON"}
+
+        return guiding_config
+
+    def _build_configuration(self, duration):
+        return {
+            "type": "REPEAT_EXPOSE",
+            "repeat_duration": (duration - 12 * u.min).sec,
+            "instrument_type": self.cleaned_data["instrument_type"],
+            "target": self._build_target_fields(),
+            "instrument_configs": self._build_instrument_config(),
+            "acquisition_config": self._build_acquisition_config(),
+            "guiding_config": self._build_guiding_config(),
+            "constraints": {"max_airmass": SETTINGS["max_airmass"]},
+        }
+
+    def _build_window(self):
+        # get target and transit
+        target = Target.objects.get(pk=self.cleaned_data["target_id"])
+        transit = Transit.objects.get(
+            target=target, number=self.cleaned_data["transit"]
+        )
+        contact = self.cleaned_data["contact"]
+
+        # calculate start, end and duration
+        if contact == "ingress":
+            start = Time(transit.start) - 20 * u.min
+            end = Time(transit.start) + 20 * u.min
+            duration = end - start
+        elif contact == "egress":
+            start = Time(transit.end) - 20 * u.min
+            end = Time(transit.end) + 20 * u.min
+            duration = end - start
+
+        # return it
+        return {"start": start.isot, "end": end.isot}, duration
+
+    def _build_instrument_config(self):
+        return [
+            {
+                "exposure_count": 1,
+                "exposure_time": self.cleaned_data["exposure_time"],
+                "mode": self.cleaned_data["readout_mode"],
+                "optical_elements": {"filter": self.cleaned_data["filter"]},
+            }
+        ]
+
+    def observation_payload(self):
+        # get target and transit
+        try:
+            target = Target.objects.get(pk=self.cleaned_data["target_id"])
+        except Target.DoesNotExist:
+            return {}
+        try:
+            transit = Transit.objects.get(
+                target=target, number=self.cleaned_data["transit"]
+            )
+        except Transit.DoesNotExist:
+            return {}
+
+        try:
+            is_priority = transit.target.targetextra_set.get(
+                key="Priority Proposal"
+            ).bool_value
+        except TargetExtra.DoesNotExist:
+            is_priority = False
+        proposal = PROPOSALS["priority"] if is_priority else PROPOSALS["low_priority"]
+
+        # get window
+        window, duration = self._build_window()
+
+        # build payload
+        payload = {
+            "name": f"{target.name} #{transit.number} {self.cleaned_data['contact'].upper()}",
+            "proposal": proposal,
+            "ipp_value": self.cleaned_data["ipp_value"],
+            "operator": "SINGLE",
+            "observation_type": "NORMAL",
+            "requests": [
+                {
+                    "configurations": [self._build_configuration(duration)],
+                    "windows": [window],
+                    "location": self._build_location(),
+                }
+            ],
+        }
+        return payload
+
+
 class IAGTransitFacility(IAGFacility):
     """
     The ``LCOFacility`` is the interface to the Las Cumbres Observatory Observation Portal. For information regarding
