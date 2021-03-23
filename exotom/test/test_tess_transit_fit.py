f3e18add52eb26e834e84944338a337458a7ebd1
Tilman Masur <t.masur@stud.uni-goettingen.de>
2021-03-23 15:29:27 +0100

    updated and extended test for fits

---
+++ exotom/test/test_tess_transit_fit.py
@@ -0,0 +1,92 @@
+from unittest.mock import MagicMock
+
+import pandas as pd
+from astropy.time import Time
+
+from django.test import TestCase
+
+from exotom.models import Target, Transit
+from exotom.transits import calculate_transits_during_next_n_days
+
+from exotom.tess_transit_fit import TessTransitFit
+
+
+class Test(TestCase):
+    def setUp(self) -> None:
+        self.data_file1 = "exotom/test/test_tess_transit_fit_data/TOI_1809.01_transit_79_light_curve_best.csv"
+        self.data_file2 = "exotom/test/test_tess_transit_fit_data/TOI_1844.01_transit_105_light_curve_best.csv"
+
+        target1_dict = {
+            "name": "test_TOI 1809.01",
+            "type": "SIDEREAL",
+            "ra": 183.3660,
+            "dec": 23.0557,
+        }
+        target1_extra_fields = {
+            "Priority Proposal": False,
+            "Mag (TESS)": 11.5384,
+            "Epoch (BJD)": 2458902.718492,
+            "Period (days)": 4.617208,
+            "Duration (hours)": 3.588807,
+            "Depth (mmag)": 12.207647,
+            "Stellar Distance (pc)": 321.084,
+            "Stellar Radius (R_Sun)": 1.1136,
+            "Planet Radius (R_Earth)": 12.041519,
+        }
+        self.target1 = Target(**target1_dict)
+        self.target1.save(extras=target1_extra_fields)
+
+        target2_dict = {
+            "name": "test_TOI 1844.01",
+            "type": "SIDEREAL",
+            "ra": 209.3886,
+            "dec": 43.4933,
+        }
+        target2_extra_fields = {
+            "Priority Proposal": False,
+            "Mag (TESS)": 11.7551,
+            "Epoch (BJD)": 2458930.329998,
+            "Period (days)": 3.212983,
+            "Duration (hours)": 2.361831,
+            "Depth (mmag)": 25.410942,
+            "Stellar Distance (pc)": 142.751,
+            "Stellar Radius (R_Sun)": 0.703594,
+            "Planet Radius (R_Earth)": 10.690659,
+        }
+        self.target2 = Target(**target2_dict)
+        self.target2.save(extras=target2_extra_fields)
+
+        # create transit object by running calculate_transits_during_next_n_days
+        self.test_now = Time("2021-02-21T15:00:00")
+        Time.now = MagicMock(return_value=self.test_now)
+
+        calculate_transits_during_next_n_days(self.target1, 1)
+        calculate_transits_during_next_n_days(self.target2, 1)
+
+    def test_fit(self):
+        # check that transit #79/105 was created
+        self.transit1 = Transit.objects.get(target=self.target1, number=79)
+        self.transit2 = Transit.objects.get(target=self.target2, number=105)
+
+        light_curve_df1 = pd.read_csv(self.data_file1)
+        light_curve_df2 = pd.read_csv(self.data_file2)
+
+        tess_transit_fit1 = TessTransitFit(light_curve_df1, self.transit1)
+        params1, c1, _ = tess_transit_fit1.make_simplest_fit()
+
+        self.assertAlmostEqual(params1.a, 1.31923941e01, places=2)
+        self.assertAlmostEqual(params1.per, 4.68757298e00, places=2)
+        self.assertAlmostEqual(params1.inc, 8.99998646e01, places=2)
+        self.assertAlmostEqual(params1.ecc, 1.39460377e-03, places=2)
+        self.assertAlmostEqual(params1.w, 7.48922400e01, places=2)
+        self.assertAlmostEqual(c1, 5.55937105e-03, places=2)
+
+        tess_transit_fit2 = TessTransitFit(light_curve_df2, self.transit2)
+        params2, c2, _ = tess_transit_fit2.make_simplest_fit()
+
+        self.assertAlmostEqual(params2.a, 1.71652884e01, places=2)
+        self.assertAlmostEqual(params2.per, 3.63653789e00, places=2)
+        self.assertAlmostEqual(params2.inc, 8.99999936e01, places=2)
+        self.assertAlmostEqual(params2.ecc, 7.72140370e-04, places=2)
+        self.assertAlmostEqual(params2.w, 9.75883643e01, places=2)
+        self.assertAlmostEqual(c2, 3.09881769e-03, places=2)
--- exotom/test/test_transit_processor.py
+++ exotom/test/test_transit_processor.py
@@ -1,5 +1,8 @@
 import os
+from unittest.mock import MagicMock
+
 import pandas as pd
+from astropy.time import Time
 
 from django.core.files import File
 from django.test import TestCase
@@ -8,15 +11,26 @@ from tom_dataproducts.models import DataProductGroup, DataProduct
 from tom_observations.models import ObservationRecord
 
 from exotom import transit_processor
-from exotom.models import Target
+from exotom.models import Target, Transit
+from exotom.transits import calculate_transits_during_next_n_days
 
 
 class Test(TestCase):
     def setUp(self) -> None:
-        self.data_dir = "exotom/test/test_transit_processor_data"
-        self.file_paths = [
-            os.path.join(self.data_dir, filename)
-            for filename in os.listdir(self.data_dir)
+        pass
+
+    def tearDown(self) -> None:
+        print("Deleting all data product files")
+        for dp in DataProduct.objects.all():
+            os.remove(dp.data.path)
+
+    def test_simple_processing_without_fit(self):
+        # no transit object is created, so no fit is done
+
+        # given
+        data_dir = "exotom/test/test_transit_processor_data_short"
+        file_paths = [
+            os.path.join(data_dir, filename) for filename in os.listdir(data_dir)
         ]
 
         target1_dict = {
@@ -25,57 +39,38 @@ class Test(TestCase):
             "ra": 340.08462499999996,
             "dec": 69.50373055555555,
         }
-        # target1_extra_fields = {
-        #     "Priority Proposal": False,  # <-- set to false!
-        #     "Depth (mmag)": 19.323865,
-        #     "Depth (mmag) err": 0.130853,
-        #     "Duration (hours)": 2.230884,
-        #     "Duration (hours) err": 0.021004,
-        #     "Epoch (BJD)": 2458899.476842,
-        #     "Epoch (BJD) err": 0.000252,
-        #     "Mag (TESS)": 11.6281,
-        #     "Period (days)": 1.327352,
-        #     "Period (days) err": 2.1e-05,
-        # }
-        self.target1 = Target(**target1_dict)
-        self.target1.save()  # extras=target1_extra_fields)
 
-        self.obs_id = 9876
-        self.transit_number = 1234
-        self.obs_record = ObservationRecord.objects.create(
-            target=self.target1,
+        target1 = Target(**target1_dict)
+        target1.save()  # extras=target1_extra_fields)
+
+        obs_id = 9876
+        transit_number = 1234
+        obs_record = ObservationRecord.objects.create(
+            target=target1,
             facility="IAGTransit",
-            observation_id=self.obs_id,
-            parameters={"transit": self.transit_number},
+            observation_id=obs_id,
+            parameters={"transit": transit_number},
         )
 
-        self.transit_dp_group = DataProductGroup(
-            name=f"Target {self.target1.name}, transit #1234"
+        transit_dp_group = DataProductGroup(
+            name=f"Target {target1.name}, transit #1234"
         )
-        self.transit_dp_group.save()
-        for file_path in self.file_paths:
+        transit_dp_group.save()
+        for file_path in file_paths:
             dp = DataProduct.objects.create(
-                target=self.target1,
-                observation_record=self.obs_record,
+                target=target1,
+                observation_record=obs_record,
                 data_product_type="image_photometry_catalog",
             )
-            dp.group.add(self.transit_dp_group)
+            dp.group.add(transit_dp_group)
             with open(file_path) as f:
                 dp.data.save(os.path.basename(file_path), File(f))
             dp.save()
             print(dp.data)
 
-        self.transit_processor = transit_processor.TransitProcessor(
-            self.transit_dp_group
-        )
+        transit_processorr = transit_processor.TransitProcessor(transit_dp_group)
 
-    def tearDown(self) -> None:
-        print("Deleting all data product files")
-        for dp in DataProduct.objects.all():
-            os.remove(dp.data.path)
-
-    def test_simple_processing(self):
-        self.transit_processor.process_transit_dataproductgroup()
+        transit_processorr.process()
 
         photometry_cat_dps = DataProduct.objects.filter(
             data_product_type="image_photometry_catalog"
@@ -88,11 +83,92 @@ class Test(TestCase):
         )
         image_file_dps = DataProduct.objects.filter(data_product_type="image_file")
 
-        self.assertEqual(len(photometry_cat_dps), len(self.file_paths))
+        self.assertEqual(len(photometry_cat_dps), len(file_paths))
         self.assertEqual(len(transit_all_light_curve_dps), 1)
         self.assertEqual(len(transit_best_light_curve_dps), 1)
         self.assertEqual(len(image_file_dps), 1)
 
         transit_light_curve_dp = transit_best_light_curve_dps[0]
         light_curves_df = pd.read_csv(transit_light_curve_dp.data.path)
-        self.assertEqual(light_curves_df.shape[0], len(self.file_paths))
+        self.assertEqual(light_curves_df.shape[0], len(file_paths))
+
+    def test_long_processing_with_fit(self):
+        # given
+        data_dir = "exotom/test/test_transit_processor_data_long_with_fit"
+        file_paths = [
+            os.path.join(data_dir, filename) for filename in os.listdir(data_dir)
+        ]
+
+        target1_dict = {
+            "name": "test_TOI 1809.01",
+            "type": "SIDEREAL",
+            "ra": 183.3660,
+            "dec": 23.0557,
+        }
+        target1_extra_fields = {
+            "Priority Proposal": False,
+            "Mag (TESS)": 11.6281,
+            "Epoch (BJD)": 2458902.718492,
+            "Duration (hours)": 3.588807,
+            "Period (days)": 4.617208,
+            "Depth (mmag)": 12.207647,
+            "Planet Radius (R_Earth)": 12.041519,
+            "Stellar Radius (R_Sun)": 1.1136,
+            "Stellar Distance (pc)": 321.084,
+        }
+        target1 = Target(**target1_dict)
+        target1.save(extras=target1_extra_fields)
+
+        # create transit object by running calculate_transits_during_next_n_days
+        test_now = Time("2021-02-21T15:00:00")
+        Time.now = MagicMock(return_value=test_now)
+        calculate_transits_during_next_n_days(target1, 1)
+        # make sure transit #79 was created
+        transit1 = Transit.objects.get(target=target1, number=79)
+
+        obs_id = 9876
+        transit_number = 1234
+        obs_record = ObservationRecord.objects.create(
+            target=target1,
+            facility="IAGTransit",
+            observation_id=obs_id,
+            parameters={"transit": transit_number, "transit_id": transit1.id},
+        )
+
+        transit_dp_group = DataProductGroup(name=f"Target {target1.name}, transit #79")
+        transit_dp_group.save()
+        for file_path in file_paths:
+            dp = DataProduct.objects.create(
+                target=target1,
+                observation_record=obs_record,
+                data_product_type="image_photometry_catalog",
+            )
+            dp.group.add(transit_dp_group)
+            with open(file_path) as f:
+                dp.data.save(os.path.basename(file_path), File(f))
+            dp.save()
+            print(dp.data)
+
+        transit_processorr = transit_processor.TransitProcessor(transit_dp_group)
+
+        transit_processorr.process()
+
+        photometry_cat_dps = DataProduct.objects.filter(
+            data_product_type="image_photometry_catalog"
+        )
+        transit_all_light_curve_dps = DataProduct.objects.filter(
+            data_product_type="transit_all_light_curves"
+        )
+        transit_best_light_curve_dps = DataProduct.objects.filter(
+            data_product_type="transit_best_light_curves"
+        )
+        image_file_dps = DataProduct.objects.filter(data_product_type="image_file")
+
+        self.assertEqual(len(photometry_cat_dps), len(file_paths))
+        self.assertEqual(len(transit_all_light_curve_dps), 1)
+        self.assertEqual(len(transit_best_light_curve_dps), 1)
+        self.assertEqual(len(image_file_dps), 1)
+
+        transit_light_curve_dp = transit_best_light_curve_dps[0]
+        light_curves_df = pd.read_csv(transit_light_curve_dp.data.path)
+        self.assertEqual(light_curves_df.shape[0], len(file_paths))
